c17b2ba8589f270ff1e9f769fed4b28992b419c4
Hiroaki Saito <bus.hirosnsd1220@gmail.com>
2021-06-06 22:32:39 +0900

    add HTML/JS/CS and images

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 // JavaScript Document
 var button = document.querySelectorAll('.l-button');
 button = Array.from(button);
-console.log(button);
-
-
 button.forEach(function(target){
     target.addEventListener('click',()=>{
         var mark = document.querySelectorAll('.mark');
@@ -26,13 +23,71 @@ const hoverarea = document.getElementById('sample-outer');
 const sample1 = document.getElementById('sample1');
 hoverarea.addEventListener('mousemove',(event)=>{
 
-    var axisX = (window.innerWidth/2 - event.pageX)/10;
-    var axisY = (window.innerHeight/2 - event.pageY)/10;
+    var axisX = (window.innerWidth/2 - event.pageX)/90;
+    var axisY = (window.innerHeight/2 - event.pageY)/90;
   
-    sample1.style.transform = `rotateX(${axisX}deg) rotateY(${axisY}deg)`;
+    sample1.style.transform = `rotateX(${-axisX}deg) rotateY(${-axisY}deg)`;
+    sample1.style.transition = "all .5s linear";
 });
+var text3d = document.querySelectorAll('.popout');
 
-hoverarea.addEventListener('mouseout',(event)=>{
+hoverarea.addEventListener('mousemove',(event)=>{
+    text3d.forEach((target)=>{
+        target.style.transform = " translateY(10px) translateZ(70px)";
+    });
+});
+hoverarea.addEventListener('mouseleave',(event)=>{
     sample1.style.transform = "rotateX(0deg) rotateY(0deg)";
+    text3d.forEach((target)=>{
+        target.style.transform = "translateZ(0)";
+        target.style.transition = "all .5s linear";
+    });
 });
 
+function showTime(){
+    const now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes() ;
+    var time = document.getElementById('time');
+    if(hours < 10 && minutes < 10){
+        time.innerHTML = `0${hours}&nbsp;0${minutes}`;
+    }else if(hours<10){
+        time.innerHTML = `0${hours}&nbsp;${minutes}`;
+    }else if(minutes<10){
+        time.innerHTML = `${hours}&nbsp;0${minutes}`;
+    }else{
+        time.innerHTML = `${hours}&nbsp;${minutes}`;
+    }
+};
+setInterval('showTime()',1000);
+
+var buttonToOpen = document.getElementById('jsOpenTrigger');
+// console.log(sampleBlock)
+// console.log(opentab);
+
+buttonToOpen.addEventListener('click',()=>{
+    var jsClickOpen = document.getElementById('js-clickOpen');
+    var innerBtn = document.getElementById('innerbtn');
+    jsClickOpen.classList.toggle('hideBoxShow');
+    innerBtn.classList.toggle('open');
+});
+
+var Height = window.innerHeight;
+window.addEventListener('scroll',()=>{
+    var text = document.querySelectorAll('.hidetext');
+    text.forEach((target)=>{
+        var targetHeight = target.getBoundingClientRect().top;
+        if(targetHeight < 3 * Height / 4){
+            target.classList.add('showtext');
+        }
+    });
+
+    var hugeText = document.querySelectorAll('.slidedown');
+    hugeText.forEach((hugetarget)=>{
+        var hugeTargetHeight = hugetarget.getBoundingClientRect().top;
+        if(hugeTargetHeight < 3 * Height / 4){
+            hugetarget.classList.add('slideup');
+        }
+    });
+    
+});
